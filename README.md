20ddc93dc3845a80b40a50ec609a1f5713b61c84
jonathangeovani <dongeovanimenor@gmail.com>
2022-05-01 22:27:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gerador_de_senhas
+ Um exemplo simples da utilização da biblioteca Random para gerar senhas.
---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+letrasmin = 'abcdefghijklmnopqrstuvwxyz'
+letrasmai = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numeros = '0123456789'
+simbolos = '{}[].,;#_-'
+
+possiveis = letrasmin + letrasmai + numeros + simbolos
+numcaracteres = int(input('Deseja gerar uma senha de quantos caracteres?\n-> '))
+senha = ''.join(random.sample(possiveis, numcaracteres))
+
+print('='*30)
+print(f'Senha gerada -> {senha}\n')
