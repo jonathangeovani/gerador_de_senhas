9afa9da4755b09c4cfe454ac461deaa830fc1d11
jonathangeovani <dongeovanimenor@gmail.com>
2022-11-21 22:57:27 -0300

    add mai simbolos

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ os.system('cls')
 letrasmin = 'abcdefghijklmnopqrstuvwxyz'
 letrasmai = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numeros = '0123456789'
-simbolos = '{}[].,;#_-'
+simbolos = '{}[].,;#_-?!=<>@$&'
 
 possiveis = letrasmin + letrasmai + numeros + simbolos
 numcaracteres = int(input('Deseja gerar uma senha de quantos caracteres?\n-> '))
 senha = ''.join(random.sample(possiveis, numcaracteres))
 
 print('='*30)
-print(f'Senha gerada -> {senha}\n')
+print(f'Senha gerada:\n{senha}\n')
